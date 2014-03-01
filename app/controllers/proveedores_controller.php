fe22b1798da2c699835cdb2b6fd2d1acecddb21e
manuel@elarte.coop <manuel@elarte.coop>
2014-03-01 16:50:07 +0100

    Subinto la plantilla crear.phtml

--- app/controllers/proveedores_controller.php
+++ app/controllers/proveedores_controller.php
@@ -7,11 +7,42 @@ Load::models('proveedores');
 class ProveedoresController extends AppController {
 	 
 	 /**
-     * Obtiene una lista para paginar los menus
+     * Obtiene una lista para paginar los proveedores
      */
     public function index($page=1)
     {
         $proveedor = new Proveedores();
         $this->listProveedores = $proveedor->getProveedores($page);
     }
+    
+    /**
+     * Crea un Registro
+     */
+
+	public function crear()
+    {
+        /**
+         * Se verifica si el usuario envio el form (submit) y si ademas 
+         * dentro del array POST existe uno llamado "menus"
+         * el cual aplica la autocarga de objeto para guardar los 
+         * datos enviado por POST utilizando autocarga de objeto
+         */
+        if(Input::hasPost('proveedor')){
+            /**
+             * se le pasa al modelo por constructor los datos del form y ActiveRecord recoge esos datos
+             * y los asocia al campo correspondiente siempre y cuando se utilice la convención
+             * model.campo
+             */
+            $proveedor = new Proveedores(Input::post('proveedor'));
+            //En caso que falle la operación de guardar
+            if($proveedor->create()){
+                Flash::valid('Operación exitosa');
+                //Eliminamos el POST, si no queremos que se vean en el form
+                Input::delete();
+                return;               
+            }else{
+                Flash::error('Falló Operación');
+            }
+        }
+	}
 }
--- app/models/proveedores.php
+++ app/models/proveedores.php
@@ -6,7 +6,7 @@ class Proveedores extends ActiveRecord {
      * Retorna los proveedores para ser paginados
      *
      */
-    public function getProveedores($page, $ppage=20)
+    public function getProveedores($page, $ppage=10)
     {
         return $this->paginate("page: $page", "per_page: $ppage", 'order: id desc');
     }
